186af2feca20a64cc1f60f0c2b120b991db49f45
Luidmyla Karmeliuk <lkarm67@gmail.com>
2025-07-16 13:42:41 +0300

    fix snackbar fils: hnml, js; css

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,43 @@
-console.log('Snackbar');
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+  const isSuccess = state === 'fulfilled';
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isSuccess) {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        icon: '',
+      });
+      console.log(`✅ Fulfilled promise in ${delay}ms`);
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        icon: '',
+      });
+      console.log(`❌ Rejected promise in ${delay}ms`);  
+    });
+
+  form.reset();
+});
